0c5861a6518871451bcf8ce97c19fde05d40c252
HL <hoanglinh171038@gmail.com>
2024-03-24 11:04:47 +0700

    46.reffix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import authOptions from "./options";
 import NextAuth from "next-auth";
+import authOptions from "./options";
+
 
 const handler = NextAuth(authOptions);
 export {handler as GET, handler as POST}
